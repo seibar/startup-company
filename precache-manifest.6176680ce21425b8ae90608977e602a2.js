4d4058e160d426935d8f9636c38fa8b7f263a602
AJ Rabe <aj.rabe@spok.com>
2020-10-16 00:03:13 -0500

    Updates

---
+++ precache-manifest.6176680ce21425b8ae90608977e602a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e89096aca2517108d1a6ef9c404e27",
+    "url": "/startup-company/index.html"
+  },
+  {
+    "revision": "19faad944bef90240c6e",
+    "url": "/startup-company/static/css/main.fd6ab1a7.chunk.css"
+  },
+  {
+    "revision": "632d260b4dc5b9a1590b",
+    "url": "/startup-company/static/js/2.e0577d11.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/startup-company/static/js/2.e0577d11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19faad944bef90240c6e",
+    "url": "/startup-company/static/js/main.6f14904c.chunk.js"
+  },
+  {
+    "revision": "e80e99c86c1ad243a2f7",
+    "url": "/startup-company/static/js/runtime-main.8627cc3f.js"
+  }
+]);
